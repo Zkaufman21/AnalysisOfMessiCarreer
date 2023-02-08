53e85c3359aa1ea413becc6a150e21f3bee61cf7
Zak Kaufman <Zak3148@rit.edu>
2023-02-08 17:32:36 -0500

    Created initial script for analysis

---
+++ NufcMarketValueAnalysis.R
@@ -0,0 +1,7 @@
+### Zak Kaufman
+### 2/8/23
+### This will be an attempt to analyze Newcastle United's 
+### player's market trends and specifically the effect the 
+### new owners had on the market value.
+
+#I am using free data found from 
