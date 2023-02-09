4b2af7689426c8859ef14a166bcde9ea0d5dc4c5
Zak Kaufman <Zak3148@rit.edu>
2023-02-08 19:42:52 -0500

    Final Main Scatter Plot will move onto analysis next

--- NufcMarketValueAnalysis.R
+++ NufcMarketValueAnalysis.R
@@ -26,9 +26,14 @@ player_valuations$total_time <- as.numeric(substring(player_valuations$date, 1,4
 relevant_newcastle_players <- newcastle_players[newcastle_players$last_season >2021, ]
 
 # plot newcastle players as time has gone on by their valuation 
-ggplot(player_valuations[player_valuations$total_time > 2015,], 
+whole_nufc_team_scatterplot<- ggplot(player_valuations[player_valuations$total_time > 2015,], 
        aes(total_time, market_value_in_eur/1000000, 
            color = factor(relevant_newcastle_players$name[match(player_id, 
                                                                 relevant_newcastle_players$player_id)])))+
   geom_point(alpha = .5)+
   scale_colour_discrete(na.translate = F)
+
+whole_nufc_team_scatterplot + labs(title = "Relevant Newcastle Players Market Value Growth",
+                                     color = "Players",
+                                     x = "Date (Takeover Occured Oct 7, 2021)",
+                                     y = "Market Value (Millions of Euros")
