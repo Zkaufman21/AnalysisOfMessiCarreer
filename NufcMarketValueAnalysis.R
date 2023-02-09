a9131fd0f50244dc21e24006286eb63ea8bf6265
Zak Kaufman <Zak3148@rit.edu>
2023-02-08 19:11:54 -0500

    Finished up a basic level scatterplot of market values for all newcastle players who have played in the most recent year

--- NufcMarketValueAnalysis.R
+++ NufcMarketValueAnalysis.R
@@ -16,17 +16,20 @@ newcastle_players <- players[players$current_club_name == "Newcastle United", ]
 # creating a player evaluations subset of just newcastle players
 newcastle_player_valuations <- player_valuations[player_valuations$current_club_id == 762, ]
 
-##adding a column for time using a numerical value in nufc valuations
+# adding a column for time using a numerical value in nufc valuations
 newcastle_player_valuations$total_time <- as.numeric(substring(newcastle_player_valuations$date, 1,4))+
   as.numeric(substring(newcastle_player_valuations$date,6,7))/12 +
   as.numeric(substring(newcastle_player_valuations$date,9,10))/365
 
-# add player names to player valuations using mutate()
-
+# removing irrelevant players to this data by eliminating players who were not on the team at to 05/30/2022
+relevant_newcastle_players <- newcastle_players[newcastle_players$last_season >2021, ]
 
 # plot newcastle players as time has gone on by their valuation 
 ggplot(newcastle_player_valuations[newcastle_player_valuations$total_time > 2015,], 
        aes(total_time, market_value_in_eur/1000000, 
-           color = factor(newcastle_players$name[match(player_id, newcastle_players$player_id)])))+
-  geom_point(alpha = .5)
+           color = factor(relevant_newcastle_players$name[match(player_id, 
+                                                                relevant_newcastle_players$player_id)])))+
+  geom_point(alpha = .5)+
+  scale_colour_discrete(na.translate = F)
 
+# plot sum
